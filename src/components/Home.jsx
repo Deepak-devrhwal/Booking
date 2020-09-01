2c48ad719780800dec77c6c2f68230686ec03e1e
Deepak Devrhwal <deepakdevrhwal@gmail.com>
2020-09-01 09:23:48 +0530

    ..

---
+++ src/components/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+import styles from "./../styles/home.module.scss";
+
+export default () => {
+  return (
+    <div className={styles.home_container}>
+      <h1> Welcome Here!! </h1>
+      <div className={styles.links_container}>
+        <Link className={styles.links} to='/login'>
+          <Button variant='contained' color='primary'>
+            Login
+          </Button>
+        </Link>
+        <Link className={styles.links} to='/bookings'>
+          <Button variant='contained' color='primary'>
+            Bookings Dashboard
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
